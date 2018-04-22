5c19313e684a7198fcdfe1c76381799a66015fe2
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-22 03:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if (string.)
+  } else if (string === )
   }
   }
